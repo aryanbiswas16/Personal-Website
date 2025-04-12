3b53526f7cd8797d40565fabee8727131abf5eec
Aryan Biswas <104274026+aryanbiswas16@users.noreply.github.com>
2025-04-12 18:15:03 -0400

    Good enough for now

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -23,21 +23,22 @@ class Intro extends React.Component {
     return (
       <div id="intro">
         <img src={avatarImage} alt="Avatar" className="intro-avatar" />
-        <span className="intro-title">
-          hi, <span className="intro-name">Aryan</span> here.
-        </span>
-        <div className="intro-subtitle">I create stuff sometimes.</div>
-        <div className="intro-desc">
-          I'm a software engineer based in Toronto. I'm passionate about
-          building software that makes a difference. I focus on creating
-          efficient, scalable, and user-friendly solutions.
+        <div className="intro-text-content">
+          <div className="intro-title-container">
+            <span>hi,</span>
+            <span className="intro-name">Aryan</span>
+            <span>here.</span>
+          </div>
+          <div className="intro-subtitle">I create stuff sometimes.</div>
+          <div className="intro-desc">
+            I'm a software engineer based in Toronto. I'm passionate about
+            building software that makes a difference. I focus on creating
+            efficient, scalable, and user-friendly solutions.
+          </div>
+          <a href="mailto:aryanbiswas16@gmail.com" className="intro-contact">
+            Say hi!
+          </a>
         </div>
-        <a
-          href="mailto:aryanbiswas16@gmail.com"
-          className="intro-contact"
-        >
-          Say hi!
-        </a>
       </div>
     );
   }
