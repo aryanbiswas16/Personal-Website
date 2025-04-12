21f43e0624336aa720a19ca15c576901c94a9a66
Aryan Biswas <104274026+aryanbiswas16@users.noreply.github.com>
2025-04-12 19:02:00 -0400

    navigation working

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -14,7 +14,7 @@ function Header() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#intro">Home</Nav.Link>
             <Nav.Link href="#about">About</Nav.Link>
             <Nav.Link href="#experience">Experience</Nav.Link>
             <Nav.Link href="#projects">Projects</Nav.Link>
